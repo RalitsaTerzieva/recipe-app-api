660cecadfab1f06db99aaf5f3e68e6d66ae5e80a
Ralitsa Popova <ralicaterzieva90@gmail.com>
2023-05-22 15:28:23 +0300

    Add wait_for_db command

--- app/core/tests/test_commans.py
+++ app/core/tests/test_commans.py
@@ -15,17 +15,13 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
-
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wai_for_db_delay(self, patched_sleep, patched_check):
         """ Test waiting for db when getting OperationalError """
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-        [OperationalError] * 3 + [True]
-
+            [OperationalError] * 3 + [True]
         call_command('wait_for_db')
-
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
-
+        patched_check.assert_called_with(databases=['default'])
