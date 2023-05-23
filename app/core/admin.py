d2933c238aef009181ab1de32256fb09dcc2ba62
Ralitsa Popova <ralicaterzieva90@gmail.com>
2023-05-23 20:31:50 +0300

    Add test and change the behaviour of the user admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (
+            _('Important dates'),
+            {'fields': ('last_login',)}
+        ),
+    )
+    readonly_fields = ['last_login']
+
+
+admin.site.register(User, UserAdmin)
