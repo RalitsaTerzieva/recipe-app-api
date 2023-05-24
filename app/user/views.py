048c71f98ca931b4bca718df9b4407fe52fc5ffe
Ralitsa Popova <ralicaterzieva90@gmail.com>
2023-05-24 14:17:19 +0300

    Add serializer and url for it

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render # noqa
+"""
+Views for the user API
+"""
 
-# Create your views here.
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """ Create a new user in the system """
+    serializer_class = UserSerializer
