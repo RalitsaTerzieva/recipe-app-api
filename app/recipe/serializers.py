78b90c0ccf55c088bb3a5a32d1afe0dbc6b19970
Ralitsa Popova <ralicaterzieva90@gmail.com>
2023-05-25 18:42:16 +0300

    Add recipe api logic

---
+++ app/recipe/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = [
+            'id',
+            'title',
+            'time_minutes',
+            'price',
+            'description',
+            'link']
+        read_only_fields = ['id']
