720ae5e8dcf6bd62c15274552f8892dfdb3e2f11
Ralitsa Popova <ralicaterzieva90@gmail.com>
2023-05-25 15:13:55 +0300

    Add tests and logic manage user api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,6 +22,15 @@ class UserSerializer(serializers.ModelSerializer):
         """ Create and return a user with encrypted password """
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField()
--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user API
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -21,3 +21,13 @@ class CreateTokenView(ObtainAuthToken):
     """ Create new auth token for user """
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """ Retrieve and return the authenticated user """
+        return self.request.user
