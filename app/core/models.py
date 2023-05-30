eda44a178e2bc4585ee9d9e700bc18ae295660ca
Ralitsa Popova <ralicaterzieva90@gmail.com>
2023-05-30 15:14:23 +0300

    Add ImageField and makemigrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -5,6 +8,14 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+
+
+def recipe_file_image_path(instance, filename):
+    """Generate file path for a new recipe image"""
+    extension = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{extension}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -55,6 +66,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_file_image_path)
 
     def __str__(self):
         return self.title
